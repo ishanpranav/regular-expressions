e5ccddec0cfadb0647a9dda0ac40899ca4e1ab90
Ishan Pranav <ishan.pranav@outlook.com>
2025-09-15 22:35:48 -0400

    Add working dollar regex

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Regular Expressions
 
-This is a pair of regular expression programs implemented in C\# for the NYU CSCI 469 Natural Language Processing course and distributed under the MIT license.
+This is a pair of regular expression programs implemented in C\# for the NYU
+CSCI 469 Natural Language Processing course and distributed under the MIT
+license.
+
+## Usage
+
+```sh
+./DollarRegex < test_dollar_phone_corpus.txt > output.txt
+```
+
+```sh
+./PhoneRegex < test_dollar_phone_corpus.txt > output.txt
+```
 
 ## License
 
---
+++ src/DollarRegex/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DollarRegex;
+
+internal static partial class Program
+{
+    [GeneratedRegex(
+        @"(?:\$(?:-?\d{1,3}(?:,\d{3})*|\d+)(?:\.\d+)?\s?(?:MILLION|BILLION|TRILLION)?(?:\s?(?:DOLLARS?|CENTS?))?(?:\s?AND\s?\d+\s?CENTS?)?)|(?:(?:(?:(?:(?:HALF|A|ONE|TWO|THREE|FOUR|FIVE|SIX|SEVEN|EIGHT|NINE|TEN|ELEVEN|TWELVE|THIRTEEN|FOURTEEN|FIFTEEN|SIXTEEN|SEVENTEEN|EIGHTEEN|NINETEEN|TWENTY|THIRTY|FOURTY|FIFTY|SIXTY|SEVENTY|EIGHTY|NINETY|HUNDRED|THOUSAND|MILLION|BILLION|TRILLION|AND)\s?)+)|(?:(-?\d{1,3}(?:,\d{3})*|\d+)(?:\.\d+)?\s?))(?:MILLION|BILLION|TRILLION)?(\s?(?:DOLLARS?|CENTS?))(\s?AND\s?\d+\s?CENTS?)?)",
+        RegexOptions.IgnoreCase)]
+    private static partial Regex DollarRegex();
+
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: {0} <file>", Process.GetCurrentProcess().ProcessName);
+
+            return;
+        }
+
+        string fileName = args[0];
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("File does not exist: \"{0}\".", fileName);
+
+            return;
+        }
+
+        using StreamReader reader = File.OpenText(fileName);
+
+        string? line;
+        Regex regex = DollarRegex();
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            foreach (Match match in regex.Matches(line))
+            {
+                Console.WriteLine(match.Value.Trim());
+            }
+        }
+    }
+}
